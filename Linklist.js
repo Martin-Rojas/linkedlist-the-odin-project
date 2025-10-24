89f9cd6ac9471cee6f653e83d4fcb6b2bca60447
Martin-Rojas <rojas.martin25@yahoo.com>
2025-10-23 17:56:33 -0700

    code updated for function append()

--- Linklist.js
+++ Linklist.js
@@ -11,6 +11,20 @@ class LinkedList {
     newNode.nodeNext = this.head; // new node points to old head
     this.head = newNode; // new node becomes the new head
   }
+  /* Adds a new node containing value to the end of the list*/
+  append(value) {
+    let currentNode = this.head; // set currentNode to the first Node
+    let newNode = new Node(value); // create new Node
+
+    while (currentNode !== null) {
+      if (currentNode.nodeNext === null) {
+        // if the pointer point to null
+        currentNode.nodeNext = newNode; // points to new node
+        currentNode = currentNode.nodeNext; // currentNode becomes next node on list
+      }
+      currentNode = currentNode.nodeNext;
+    }
+  }
 
   printList() {
     let currentNode = this.head;
