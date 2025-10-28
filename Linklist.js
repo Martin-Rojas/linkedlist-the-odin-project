222519731d689feaa828d75ea401bebc6bf9e9ce
Martin-Rojas <rojas.martin25@yahoo.com>
2025-10-27 18:48:50 -0700

    code update append(value) so tial/head is updated when append new node

--- Linklist.js
+++ Linklist.js
@@ -6,25 +6,35 @@ class LinkedList {
     this.tail = null; // get the last node of list
   }
 
+  /**  adds a new node containing value to the start of the list */
   prepend(value) {
     // Create new element with the new value
     let newNode = new Node(value);
     newNode.nodeNext = this.head; // new node points to old head
     this.head = newNode; // new node becomes the new head
   }
+
   /* Adds a new node containing value to the end of the list*/
   append(value) {
-    let currentNode = this.head; // set currentNode to the first Node
     let newNode = new Node(value); // create new Node
+    // Check if list is empty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let currentNode = this.head; // set currentNode to the first Node
 
-    while (currentNode !== null) {
-      if (currentNode.nodeNext === null) {
+      while (currentNode !== null) {
         // if the pointer point to null
-        currentNode.nodeNext = newNode; // points to new node
-        currentNode = currentNode.nodeNext; // currentNode becomes next node on list
+        if (currentNode.nodeNext === null) {
+          currentNode.nodeNext = newNode; // points to new node
+          this.tail = newNode; // newNode becomes tials
+          currentNode = currentNode.nodeNext; // currentNode becomes next node on list
+        }
+        currentNode = currentNode.nodeNext; // move to next node
       }
-      currentNode = currentNode.nodeNext; // move to next node
     }
+    console.log(`Tail: ${this.tail.value}  Head: ${this.head.value}`);
   }
   /*returns the total number of nodes in the list*/
   size() {
